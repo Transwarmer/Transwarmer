4996b01c524dd150e4f54e07ff70e719d01f9eb3
TyounanMOTI <unknown-user@fe80::1174:64d0:d72b:b623%10>
2013-07-20 22:25:01 +0900

    add tutolib

--- Transwarmer/Scene/GameScene/GameScene.cs
+++ Transwarmer/Scene/GameScene/GameScene.cs
@@ -1,5 +1,6 @@
 using System;
 using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.Core;
 
 namespace Transwarmer
 {
@@ -8,6 +9,7 @@ namespace Transwarmer
 		public GameScene ()
 		{
 			this.Camera.SetViewFromViewport ();
+			this.Camera2D.SetViewX (new Vector2(480.0f, 272.0f), new Vector2(0.0f, 0.0f));
 			AddChild (new BackgroundNode ());
 		}
 	}
