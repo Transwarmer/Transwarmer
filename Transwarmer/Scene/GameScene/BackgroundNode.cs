13e853bbcd5e23eff815ec3f39e15cf9d66f4bc9
TyounanMOTI <unknown-user@fe80::1174:64d0:d72b:b623%10>
2013-07-21 05:39:59 +0900

    repeat background

--- Transwarmer/Scene/GameScene/BackgroundNode.cs
+++ Transwarmer/Scene/GameScene/BackgroundNode.cs
@@ -9,26 +9,32 @@ namespace Transwarmer
 	public class BackgroundNode : Node
 	{
 		Texture2D texture;
-		SpriteUV spritePrev;
-		SpriteUV spriteNext;
+		SpriteUV[] sprites;
 		
 		public BackgroundNode ()
 		{
-			Director.Instance.GL.Context.SetClearColor(0,0,0,0);
+			Director.Instance.GL.Context.SetClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 			
-			texture = new Texture2D ("/Application/Assets/background/dummy_fullscreen.png", false);
+			texture = new Texture2D ("/Application/Assets/images/haikei.png", false);
 			TextureInfo textureInfo = new TextureInfo (texture);
 			
-			spritePrev = new SpriteUV (textureInfo);
-			spritePrev.Quad.S = textureInfo.TextureSizef;
-			
-			spriteNext = new SpriteUV (textureInfo);
-			spriteNext.Quad.S = textureInfo.TextureSizef;
-			spriteNext.Quad.T = new Vector2(960.0f, 0.0f);
-			
-			AddChild (spritePrev);
-			AddChild (spriteNext);
+			// fill screen with tree
+			sprites = new SpriteUV[3];
+			for (int i = 0; i < 3; i++) {
+				sprites[i] = generateSprite(i * 800.0f, textureInfo);
+				AddChild (sprites[i]);
+			}
+		}
+		
+		private SpriteUV generateSprite (float leftX, TextureInfo textureInfo)
+		{
+			var sprite = new SpriteUV (textureInfo);
+			float spriteHeight = 800.0f;
+			float spriteWidth = spriteHeight;
+			float screenHeight = 544.0f;
+			sprite.Quad = new TRS (new Bounds2 (new Vector2 (leftX, screenHeight / 2.0f - spriteHeight / 2.0f),
+			                                    new Vector2 (leftX + spriteWidth, screenHeight / 2.0f + spriteHeight / 2.0f)));
+			return sprite;
 		}
 	}
 }
-
