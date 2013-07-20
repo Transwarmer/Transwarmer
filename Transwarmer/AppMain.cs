432667ebfc1df9035a68bab15d70b228f53db9fe
TyounanMOTI <unknown-user@fe80::1174:64d0:d72b:b623%10>
2013-07-20 16:36:56 +0900

    initialize director

--- Transwarmer/AppMain.cs
+++ Transwarmer/AppMain.cs
@@ -5,45 +5,20 @@ using Sce.PlayStation.Core;
 using Sce.PlayStation.Core.Environment;
 using Sce.PlayStation.Core.Graphics;
 using Sce.PlayStation.Core.Input;
+using Sce.PlayStation.HighLevel.GameEngine2D;
 
 namespace Transwarmer
 {
 	public class AppMain
-	{
-		private static GraphicsContext graphics;
-		
+	{	
 		public static void Main (string[] args)
 		{
 			Initialize ();
-
-			while (true) {
-				SystemEvents.CheckEvents ();
-				Update ();
-				Render ();
-			}
 		}
-		// (^^)
-
+		
 		public static void Initialize ()
 		{
-			//test!! Set up the graphics system
-			graphics = new GraphicsContext ();
-		}
-
-		public static void Update ()
-		{
-			// Query gamepad for current state
-			var gamePadData = GamePad.GetData (0);
-		}
-
-		public static void Render ()
-		{
-			// Clear the screen
-			graphics.SetClearColor (0.0f, 0.0f, 0.0f, 0.0f);
-			graphics.Clear ();
-
-			// Present the screen
-			graphics.SwapBuffers ();
+			Director.Initialize();
 		}
 	}
 }
