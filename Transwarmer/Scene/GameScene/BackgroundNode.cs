d66ce38e0d3de7730eb46491bc952f6f00c252ea
TyounanMOTI <unknown-user@fe80::1174:64d0:d72b:b623%10>
2013-07-21 10:25:49 +0900

    repeat background

--- Transwarmer/Scene/GameScene/BackgroundNode.cs
+++ Transwarmer/Scene/GameScene/BackgroundNode.cs
@@ -9,34 +9,60 @@ namespace Transwarmer
 	public class BackgroundNode : Node
 	{
 		Texture2D texture;
-		SpriteUV[] sprites;
+		SpriteUV[] spriteRing;
+		int ringCount = 3;
+		int ringIndex;
+		TextureInfo textureInfo;
+		float frontline = 0.0f;
+		float spriteWidth = 700.0f;
 		
 		public BackgroundNode ()
 		{
-			Director.Instance.GL.Context.SetClearColor(1.0f, 1.0f, 1.0f, 0.0f);
-			
 			texture = new Texture2D ("/Application/Assets/images/haikei.png", false);
-			TextureInfo textureInfo = new TextureInfo (texture);
+			textureInfo = new TextureInfo (texture);
 			
 			// fill screen with tree
-			sprites = new SpriteUV[3];
-			for (int i = 0; i < 3; i++) {
-				sprites[i] = generateSprite(i * 800.0f, textureInfo);
-				AddChild (sprites[i]);
+			spriteRing = new SpriteUV[ringCount];
+			for (int i = 0; i < ringCount; i++) {
+				spriteRing[i] = generateSprite(frontline);
+				frontline+= spriteWidth;
+				AddChild (spriteRing[i]);
 			}
-			
-			//Scheduler.Instance.ScheduleUpdateForTarget (this);
+			ringIndex = 0;
+			Scheduler.Instance.ScheduleUpdateForTarget (this, 2, false);
 		}
 		
-		private SpriteUV generateSprite (float leftX, TextureInfo textureInfo)
+		private SpriteUV generateSprite (float leftX)
 		{
 			var sprite = new SpriteUV (textureInfo);
-			float spriteHeight = 800.0f;
-			float spriteWidth = spriteHeight;
+			float spriteHeight = spriteWidth;
 			float screenHeight = 544.0f;
 			sprite.Quad = new TRS (new Bounds2 (new Vector2 (leftX, screenHeight / 2.0f - spriteHeight / 2.0f),
 			                                    new Vector2 (leftX + spriteWidth, screenHeight / 2.0f + spriteHeight / 2.0f)));
 			return sprite;
+		}
+		
+		public override void Update (float dt)
+		{
+			base.Update (dt);
+			var cameraBounds = Parent.Camera.CalcBounds();
+			Bounds2 spriteBounds = new Bounds2 ();
+			spriteRing[ringIndex].GetContentWorldBounds(ref spriteBounds);
+			
+			float cameraFrontLine = cameraBounds.Min.X;
+			float spriteFrontLine = spriteBounds.Max.X;
+			if (spriteFrontLine < cameraFrontLine) {
+				// remove unseen sprite
+				RemoveChild (spriteRing[ringIndex], true);
+				
+				// add sprite to next of current sprites
+				spriteRing[ringIndex] = generateSprite(frontline);
+				AddChild (spriteRing[ringIndex]);
+				
+				frontline += spriteWidth;
+				ringIndex++;
+				ringIndex %= ringCount;
+			}
 		}
 	}
 }
