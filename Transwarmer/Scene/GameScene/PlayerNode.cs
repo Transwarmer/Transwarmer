39efd7db73aafe31e700d453aa06bc029c2b73d6
tatsuhiko <ruitomo.ex@gmail.com>
2013-07-21 10:44:43 +0900

    add : move x pos

--- Transwarmer/Scene/GameScene/PlayerNode.cs
+++ Transwarmer/Scene/GameScene/PlayerNode.cs
@@ -1,5 +1,6 @@
 using Sce.PlayStation.Core;
 using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.Core.Input;
 
 
 namespace Transwarmer
@@ -11,17 +12,17 @@ namespace Transwarmer
 		public InputController Input;
 		
 		private SpriteAnimation sprite;
+		private float angle = 0;
 		
 		public PlayerNode ()
 		{
-			
 			sprite = new SpriteAnimation(
 				"Application/Assets/images/unified_texture.png", 
 				"Application/Assets/images/unified_texture.xml");
 			
-			sprite.sprite.Center = new Sce.PlayStation.Core.Vector2(0.5f, 0.5f);
+			sprite.sprite.Center = new Sce.PlayStation.Core.Vector2(0.5f, 1.0f);
 			this.position = 272;
-			sprite.Position = new Vector2(128, this.position);
+			sprite.Position = new Vector2(300, this.position);
 			sprite.SetRotation(90);
 			sprite.type = SpriteAnimation.AnimationType.Serial;
 			sprite.PlayAnimation();
@@ -35,16 +36,33 @@ namespace Transwarmer
 		{
 			base.Update (dt);
 			cameraManager.OnPlayerPositionChanged (position);
+					DebugDrawTransform();
 			
 			switch( Input.getState() )
 			{
 			case InputController.CharacterState.Shrink:
 				if( sprite.isPlayed == false){
-					position += 90.0f * dt;
+					
+					var v2 = sprite.Position.Multiply( sprite.dir2Rot( angle ) );
+					position += 90.0f * dt ;
+					
+					sprite.Position += new Vector2( 0, v2.X) * dt ;
+				}else{
+					angle += GamePad.GetData(0).AnalogRightY;
+					
+					if( angle < -30) angle = -30;
+					if( angle > 30 ) angle = 30;
+					
+					sprite.SetRotation( 90 + angle );
 				}
 				sprite.isReviece = false;
 				break;
+				
 			case InputController.CharacterState.Stretch:
+				
+				if( sprite.isPlayed == false ){
+				}
+				
 				sprite.isReviece = true;
 				break;
 			}
