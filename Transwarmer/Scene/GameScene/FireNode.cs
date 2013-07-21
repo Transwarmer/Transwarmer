6e04eb033743cc75dba289257d421058aa45996a
TyounanMOTI <unknown-user@fe80::1174:64d0:d72b:b623%10>
2013-07-21 14:55:12 +0900

    firewall ok

--- Transwarmer/Scene/GameScene/FireNode.cs
+++ Transwarmer/Scene/GameScene/FireNode.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using Sce.PlayStation.Core;
 using Sce.PlayStation.Core.Audio;
+using Sce.PlayStation.Core.Graphics;
 using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.HighLevel.GameEngine2D.Base;
 
 namespace Transwarmer
 {
@@ -14,6 +16,7 @@ namespace Transwarmer
 		private float spriteWidth = 100.0f;
 		
 		public float fireFrontLine = 0.0f;
+		private SpriteUV firewallSprite;
 		
 		public FireNode ()
 		{	
@@ -24,8 +27,23 @@ namespace Transwarmer
 			
 			Scheduler.Instance.ScheduleUpdateForTarget (this, 2, false);
 			
+			generateFirewallSprite ();
 			generateSprite(spriteWidth, 0);
 			AddChild(sprite);
+		}
+		
+		private void generateFirewallSprite ()
+		{
+			var texture = new Texture2D ("/Application/Assets/images/fireWall.png", false);
+			var textureInfo = new TextureInfo (texture);
+			firewallSprite = new SpriteUV (textureInfo);
+			float spriteWidth = 500.0f;
+			float screenHeight = 544.0f;
+			float spriteHeight = screenHeight;
+			firewallSprite.Quad = new TRS (new Bounds2 (new Vector2 (-352.0f, 0.0f),
+			                                    new Vector2 (fireFrontLine + spriteWidth - 352.0f, spriteHeight)));
+			firewallSprite.BlendMode = BlendMode.Additive;
+			AddChild (firewallSprite);
 		}
 		
 		private void generateSprite (float position, int index)
@@ -42,8 +60,9 @@ namespace Transwarmer
 		public override void Update (float dt)
 		{
 			base.Update (dt);
-			fireFrontLine += 10.0f * dt;
+			fireFrontLine += 30.0f * dt;
 			sprite.Position = new Vector2(fireFrontLine, 272);
+			firewallSprite.Quad.T = firewallSprite.Quad.T.Add (new Vector2 (30.0f*dt, 0.0f));
 		}
 	}
 }
